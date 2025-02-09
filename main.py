92820207e8608405538a88125d79b2ac1110c4e7
ezloomdev <salahii00772266@gmail.com>
2025-02-09 15:43:06 -0500

    sprite creation/movement

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Clown Invaders")
 
+# Square attributes
+square_color = (255, 0, 0) # RED
+square_size = 50 # w and h of the square
+x, y = 200, 200
+
+# Speed of the square's movement
+speed = .5
+
 # Game Loop
 running = True
 while running:
@@ -18,8 +26,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+
+    # Update position based on arrow keys
+    if keys[pygame.K_LEFT]: # Move left
+        x -= speed
+    if keys[pygame.K_RIGHT]: # Move right
+        x += speed
+    if keys[pygame.K_UP]: # Move up
+        y -= speed
+    if keys[pygame.K_DOWN]: # Move down
+        y += speed
+
     # Yellow BG
     screen.fill((255, 232, 85))
+
+    # Draw the sprite
+    pygame.draw.rect(screen, square_color, (x, y, square_size, square_size))
 
     # Update the display
     pygame.display.update()
