b1ebb38dbc3dd7bce98c05414d6fe9904953062e
ezloomdev <salahii00772266@gmail.com>
2025-02-10 23:27:23 -0500

    Jumping and Sounds
    I added jump mechanic with sound. Refined code a little.

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Clown Invaders")
+pygame.display.set_caption("Bouncy Ballz")
 
+# Load  the jump sound
+jump_sound = pygame.mixer.Sound("sounds/player/jump_sound2.wav")
+jump_sound2 = pygame.mixer.Sound("sounds/player/jump_sound.wav")
 # Creating the player
-player = Player(color = (255, 248, 194), size=50, x=375, y=275, speed=.5, gravity=0.1)
+player = Player(color = (255, 248, 194), size=50, x=375, y=275, speed=.5, gravity=.007, jump_strength=2, jump_sound=jump_sound, jump_sound2=jump_sound2)
 
 # Game Loop
 running = True
 while running:
     # Event handling
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                # Just pressed key for up
+                player.jump()
 
     # Move the player
     player.move(keys)
--- player.py
+++ player.py
@@ -3,7 +3,7 @@
 import pygame
 
 class Player:
-    def __init__(self, color, size, x, y, speed, gravity):
+    def __init__(self, color, size, x, y, speed, gravity, jump_strength,jump_sound,jump_sound2):
         self.color = color
         self.size = size
         self.x = x
@@ -11,26 +11,52 @@ class Player:
         self.speed = speed
         self.gravity = gravity
         self.velocity_y = 0
+        self.jump_sound = jump_sound
+        self.jump_sound2 = jump_sound2
+        self.jumps = 0
+        self.jump_strength = jump_strength
+        self.on_ground = True
 
     def move(self, keys):
     # Updates the players position based on key 
-        if keys[pygame.K_a]: # Move left
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] : # Move left
             self.x -= self.speed
-        if keys[pygame.K_d]: # Move right
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]: # Move right
             self.x += self.speed
-        if keys[pygame.K_w]: # Move up
-            self.y -= self.speed
-        if keys[pygame.K_s]: # Move down
-            self.y += self.speed
+
+
 
         # This applys gravity
         self.velocity_y += self.gravity
         self.y += self.velocity_y
 
-        # prevent the player from falling off the screen
+        # prevent the player from falling off the screen and resets jumps
         if self.y + self.size > 600: # Screen height is 600
             self.y = 600 - self.size
             self.velocity_y = 0
+            self.jumps = 0
+            self.on_ground = True
+
+        # Prevent the player from moving out of bounds horizontally
+        if self.x < 0:
+            self.x = 0
+        if self.x + self.size > 800:
+            self.x = 800 - self.size
+
+        # Prevent the player from moving out of bounds vert
+        if self.y < 0:
+            self.y = 0
+
+    def jump(self):
+        # Double Jump logic
+        if self.jumps < 2:
+            if self.jumps == 0:
+                self.jump_sound.play()
+            else:
+                self.jump_sound2.play()
+            self.velocity_y = -self.jump_strength
+            self.jumps += 1
+            self.on_ground = False
 
     def draw(self, screen):
         # Draw the player on screen
